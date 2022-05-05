bee54217d18aef2f789706c8ba76228f5b0d81c0
BeAHappyMan <1302656106@qq.com>
2022-05-05 12:09:29 +0800

    Result类的code属性修改

--- src/main/java/com/SelectCourse/common/Result.java
+++ src/main/java/com/SelectCourse/common/Result.java
@@ -8,26 +8,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Result {
-    private String code;
+    private String errorCode;
     private String message;
     private Object data;
 
     public static Result success(){
-        return new Result(Constants.CODE_200,"",null);
+        return new Result("0","",null);
     }
+
     public static Result success(Object data){
-        return new Result(Constants.CODE_200,"",data);
+        return new Result("0","",data);
     }
-    public static Result error(String code,String message){
-        return new Result(code,message,null);
+
+
+    public static Result success(String message){
+        return new Result("0",message,null);
     }
-    public static Result success(String code,String message){
-        return new Result(code,message,null);
+    public static Result success(String message,Object data){
+        return new Result("0",message,data);
     }
-    public static Result success(String code,String message,Object data){
-        return new Result(code,message,data);
-    }
+
+
     public static Result error(){
-        return new Result(Constants.CODE_500,"",null);
+        return new Result("1","",null);
     }
+    public static Result error(String message){
+        return new Result("1",message,null);
+    }
+
 }
--- src/main/java/com/SelectCourse/controller/UserController.java
+++ src/main/java/com/SelectCourse/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.SelectCourse.controller;
 
-import com.SelectCourse.common.Constants;
 import com.SelectCourse.common.Result;
 import com.SelectCourse.pojo.Student;
 import com.SelectCourse.pojo.Teacher;
@@ -9,15 +8,12 @@ import com.SelectCourse.service.StudentDaoService;
 import com.SelectCourse.service.TeacherDaoService;
 import com.SelectCourse.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.List;
+
 
 @RestController
 public class UserController {
@@ -38,11 +34,11 @@ public class UserController {
         System.out.println(userNumber+userPassword+userType);
         User user = userDaoService.queryUserByUserNumber(userNumber);
         if(user==null)
-            return Result.error(Constants.CODE_400,"用户不存在");
+            return Result.error("用户不存在");
         if(!user.getUserPassword().equals(userPassword))
-            return Result.error(Constants.CODE_400,"密码错误");
+            return Result.error("密码错误");
         else
-            return Result.success(Constants.CODE_200,"登录成功",user);
+            return Result.success("登录成功",user);
     }
     @RequestMapping("/Register")
     public Result userRegister(HttpServletRequest request){
@@ -52,7 +48,7 @@ public class UserController {
         String userType = request.getParameter("userType");
         User user = userDaoService.queryUserByUserNumber(userNumber);
         if(user!=null)
-            return Result.error(Constants.CODE_400,"用户已经存在");
+            return Result.error("用户已经存在");
         User user1 = new User();
         user1.setUserName(userName);
         user1.setUserNumber(userNumber);
@@ -68,10 +64,10 @@ public class UserController {
             student.setStudentClass(studentClass);
             int res = studentDaoService.addStudent(student);
             if (res > 0)
-                return Result.success(Constants.CODE_200, "学生注册成功", student);
+                return Result.success("学生注册成功", student);
             else {
                 userDaoService.deleteUser(userNumber);
-                return Result.success(Constants.CODE_401, "注册失败");
+                return Result.success("注册失败");
             }
         }
         if(userType.equals("老师")) {
@@ -81,13 +77,13 @@ public class UserController {
 
             int res = teacherDaoService.addTeacher(teacher);
             if (res > 0)
-                return Result.success(Constants.CODE_200, "老师注册成功", teacher);
+                return Result.success("老师注册成功", teacher);
             else {
                 userDaoService.deleteUser(userNumber);
-                return Result.success(Constants.CODE_401, "注册失败");
+                return Result.success("注册失败");
             }
         }
-        return Result.error(Constants.CODE_400,"userType错误");
+        return Result.error("userType错误");
     }
 
 }
